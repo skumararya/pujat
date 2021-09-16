621f324d73d3a7aeeb177333f52c593ef3e31d72
Satish Kumar Arya <satish.arya@velocis.in>
2021-09-16 16:17:40 +0530

    home and booking detail
    home and booking detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutTempleComponent } from './about-temple/about-temple.component';
+import { HomeComponent } from './home/home.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 const routes: Routes = [
   
      {path: '', redirectTo: '/login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
      { path: 'abouttemple', component: AboutTempleComponent,  children: [
       {path: 'login', component: LoginComponent, outlet: 'login1'},
+      {path: 'home', component: HomeComponent},
     ]
     
     },
+    {path: 'bookindetail', component: BookingDetailComponent},
+    {path: 'home', component: HomeComponent},
      { path: '**', redirectTo: '/login' }  
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { AboutTempleComponent } from './about-temple/about-temple.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LeftmenuComponent } from './leftmenu/leftmenu.component';
-
+import { HomeComponent } from './home/home.component';
+import {MatButtonModule} from '@angular/material/button';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,16 @@ import { LeftmenuComponent } from './leftmenu/leftmenu.component';
     HeaderComponent,
     FooterComponent,
     LeftmenuComponent,
+    HomeComponent,
+    BookingDetailComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
